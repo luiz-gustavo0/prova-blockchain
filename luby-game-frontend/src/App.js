6f1bf3e52a9e16ecd18ebb34dd42b760d5063024
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2022-06-03 16:40:28 -0300

    feat: start game and get balance

--- luby-game-frontend/src/App.js
+++ luby-game-frontend/src/App.js
@@ -1,13 +1,54 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { useContractFn } from './hooks/useContractFn';
 import { useWeb3 } from './hooks/useWeb3';
 
 function App() {
-  const { contract } = useWeb3();
+  const { selectedAccount, main } = useWeb3();
+  const { getBalance, mintLbc, startGame } = useContractFn();
 
-  console.log(contract);
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    main();
+    getBalanceIndividual();
+  }, []);
+
+  console.log('selectedAccount', selectedAccount);
+
+  const init = () => {
+    startGame()
+      .then((res) => {
+        console.log(res);
+        getBalance();
+      })
+      .catch((err) => {
+        console.log('start', err);
+      });
+  };
+
+  const getBalanceIndividual = () => {
+    getBalance()
+      .then((response) => {
+        setBalance(response);
+      })
+      .catch((err) => {
+        console.log('balance individual', err);
+      });
+  };
+
   return (
     <div className='App'>
       <h1>Dapp: LubyGame</h1>
+      <h3>
+        Your Balance: <span>{balance}</span>
+      </h3>
+      <button onClick={mintLbc}>Minerar 10 LBC</button>
+
+      <div>
+        <h2>Start Game</h2>
+        <button onClick={init}>Start</button>
+      </div>
     </div>
   );
 }
--- luby-game-frontend/src/hooks/useWeb3.js
+++ luby-game-frontend/src/hooks/useWeb3.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import Web3 from 'web3';
 
 import LubyGameContract from '../contracts/LubyGame.json';
@@ -6,36 +6,34 @@ import LubyGameContract from '../contracts/LubyGame.json';
 export const useWeb3 = () => {
   const [contract, setContract] = useState({});
   const [selectedAccount, setSelectedAccount] = useState();
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
-    const main = async () => {
-      if (window.ethereum) {
-        window.ethereum
-          .request({ method: 'eth_requestAccounts' })
-          .then((accounts) => {
-            setSelectedAccount(accounts[0]);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-
-        window.ethereum.on('accountsChanged', function (accounts) {
+  const main = useCallback(async () => {
+    if (window.ethereum) {
+      window.ethereum
+        .request({ method: 'eth_requestAccounts' })
+        .then((accounts) => {
           setSelectedAccount(accounts[0]);
+        })
+        .catch((err) => {
+          console.log(err);
         });
-      }
 
-      const web3 = new Web3(window.ethereum);
-      const networkId = await web3.eth.net.getId();
-      const newContract = new web3.eth.Contract(
-        LubyGameContract.abi,
-        LubyGameContract.networks[networkId].address
-      );
+      window.ethereum.on('accountsChanged', function (accounts) {
+        setSelectedAccount(accounts[0]);
+      });
+    }
 
-      setContract(newContract);
-    };
+    const web3 = new Web3(window.ethereum);
+    const networkId = await web3.eth.net.getId();
+    const newContract = new web3.eth.Contract(
+      LubyGameContract.abi,
+      LubyGameContract.networks[networkId].address
+    );
 
-    main();
+    setContract(newContract);
+    setIsLoaded(true);
   }, []);
 
-  return { contract, selectedAccount };
+  return { main, contract, selectedAccount, isLoaded };
 };
